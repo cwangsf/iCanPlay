567f0fa5b70a14de9c7f305d93793e0d86f799b6
Cynthia Wang <cynthiazw@gmail.com>
2025-10-25 11:44:39 +0100

    Moved files around and made folders for better structure of code

--- iCanPlay/iCanPlay/App/iCanPlayApp.swift
+++ iCanPlay/iCanPlay/App/iCanPlayApp.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 @main
 struct iCanPlayApp: App {
-    var sharedModelContainer: ModelContainer = {
+        var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
         ])
